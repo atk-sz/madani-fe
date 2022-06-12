9f6696fd231682a3d9ca80a79bcea6d94a90746d
Syed Zaid <syed.zaid525@gmail.com>
2022-06-12 15:38:12 +0530

    tempale ready with bootsratp installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, Suspense } from "react";
+// import { lazily } from "react-lazily";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./views";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { auth } from "./firebase";
+// import { useDispatch } from "react-redux";
+// import { currentUser } from "./api/auth";
+// import { LoadingOutlined } from "@ant-design/icons";
 
-function App() {
+const App = () => {
+  // const dispatch = useDispatch();
+
+  // // to check firebase auth state
+
+  // useEffect(() => {
+  //   const unsubscribe = auth.onAuthStateChanged(async (user) => {
+  //     if (user) {
+  //       const idTokenResult = await user.getIdTokenResult();
+  //       currentUser(idTokenResult.token)
+  //         .then((res) => {
+  //           dispatch({
+  //             type: "LOGGED_IN_USER",
+  //             payload: {
+  //               name: res.data.name,
+  //               email: res.data.email,
+  //               role: res.data.role,
+  //               token: idTokenResult.token,
+  //               _id: res.data._id,
+  //             },
+  //           });
+  //         })
+  //         .catch((err) => console.log(err));
+  //     }
+  //   });
+
+  //   // cleanup
+  //   return () => unsubscribe();
+  // }, [dispatch]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div className="col text-center p-5 h1">Zloading</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route exact path="/" element={Home} /> */}
+      </Routes>
+    </Suspense>
   );
-}
+};
 
 export default App;
